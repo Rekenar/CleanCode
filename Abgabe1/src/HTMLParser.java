1e3019ff6ac64a314e43a64e1aebee3d23a908d6
Philipp Fößl <PhilippFoessl@outlook.com>
2021-04-10 13:57:53 +0200

    Web Crawler 1.0

---
+++ Abgabe1/src/HTMLParser.java
@@ -0,0 +1,84 @@
+import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashSet;
+
+public class HTMLParser {
+    private HashSet<String> links;
+    private static final int MAX_DEPTH = 2;
+    private String content;
+    private String fileName;
+
+    public HTMLParser(String fileName) {
+        links = new HashSet<>();
+        this.fileName = fileName;
+        this.content = "";
+    }
+
+    public void getLinksFromURL(String URL, int depth) {
+        if (!links.contains(URL) && depth <= MAX_DEPTH && checkForResponse(URL)) {
+            System.out.println(URL + " " + depth);
+            try {
+                links.add(URL);
+                Document doc = Jsoup.connect(URL).get();
+                appendContent(URL, doc);
+
+                Elements linksOnPage = doc.select("a[href]");
+
+                depth++;
+                for (Element page : linksOnPage) {
+                    getLinksFromURL(page.attr("abs:href"), depth);
+                }
+            } catch (IOException e) {
+                System.err.println("For '" + URL + "': " + e.getMessage());
+            }
+        }
+    }
+
+    public void writeHTMLToFile() {
+        try {
+            FileWriter fileWriter = new FileWriter(this.fileName);
+            fileWriter.append(this.content);
+            fileWriter.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void appendContent(String URL, Document doc) {
+        content += "URL: " + URL +
+                "\nWordCount: " + countWords(doc) +
+                "\nImageCount: " + countSelector(doc,"img") +
+                "\nVideoCount: " + countSelector(doc,"video") + "\n";
+    }
+
+    private int countSelector(Document doc, String selector) {
+        int countSelector = 0;
+        Elements selectorOnPage = doc.getElementsByTag(selector);
+        for (Element selectorElement : selectorOnPage) {
+            countSelector++;
+        }
+        return countSelector;
+    }
+
+    private int countWords(Document doc) {
+        return doc.text().split(" ").length;
+    }
+
+    private boolean checkForResponse(String url) {
+        try {
+            Response response = Jsoup.connect(url).followRedirects(false).execute();
+            if (response.statusCode() == 404) {
+                return false;
+            }
+        } catch (IOException ex) {
+            return false;
+        }
+        return true;
+    }
+}
--- Abgabe1/src/Main.java
+++ Abgabe1/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        HTMLParser htmlParser = new HTMLParser("html.txt");
+        System.out.print("Geben Sie einen Link ein: ");
+        htmlParser.getLinksFromURL(scanner.next(),0);
+        htmlParser.writeHTMLToFile();
     }
 }
